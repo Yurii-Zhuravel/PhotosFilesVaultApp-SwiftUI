0f331f970c2a027f02501b65aaece4dff7a35e18
Yurii Zhuravel <yura.zhuravel.ios@gmail.com>
2025-09-17 17:37:04 +0300

    Readded Xcode project.

---
+++ PhotosFilesVaultApp/PhotosFilesVaultApp/PhotosFilesVaultAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosFilesVaultAppApp.swift
+//  PhotosFilesVaultApp
+//
+//  Created by Yurii Zhuravel on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotosFilesVaultAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotosFilesVaultApp/PhotosFilesVaultAppTests/PhotosFilesVaultAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosFilesVaultAppTests.swift
+//  PhotosFilesVaultAppTests
+//
+//  Created by Yurii Zhuravel on 17.09.2025.
+//
+
+import Testing
+
+struct PhotosFilesVaultAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
